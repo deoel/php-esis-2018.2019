5c38d4ceeb241e6f34b88a3023cb32db40597df1
Lwamba Gaetan <Gaetan@MacBook-Air-de-MAC.local>
2018-11-07 10:32:03 +0200

    creation du fichier index

---
+++ ExpliqueSimplement/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>ExpliaueSimplement</title>
+</head>
+<body>
+    <h1>ExpliaueSimplement</h1>
+    <h3>Se connecter</h3>
+    <form method="post" action="connexion.php">
+        <label>Pseudo : </label>
+        <input type="text" name="pseudo"/>
+        <br>
+        <label>Mot de passe : </label>
+        <input type="password" name="mdp"/>
+        <br>
+        <input type="submit" value="connection"/> <a href="compte.php"> Creer un nouveau compte </a>
+    </form>
+</body>
+</html>
